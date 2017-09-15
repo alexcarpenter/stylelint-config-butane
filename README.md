68ab75793ed03caf050df83e18299255ab09bc56
Alex Carpenter <alex@mightyinthemidwest.com>
2017-09-15 15:35:07 -0400

    Initial release

---
+++ README.md
@@ -0,0 +1,23 @@
+# stylelint-config-butane
+
+> Butane shareable config for stylelint.
+
+## Installation
+
+```bash
+$ npm install stylelint-config-butane
+```
+
+## Usage
+
+Set your stylelint config to:
+
+```json
+{
+  "extends": "styleline-config-butane"
+}
+```
+
+## License
+
+[MIT](https://opensource.org/licenses/MIT). © 2017 Alex Carpenter
---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "plugins": [
+    "stylelint-scss"
+  ],
+  "rules": {
+    "color-hex-case": "lower",
+    "color-hex-length": "short",
+    "font-weight-notation": "numeric",
+    "indentation": 2,
+    "max-empty-lines": 4,
+    "max-nesting-depth": 2,
+    "no-invalid-double-slash-comments": true,
+    "string-quotes": "single",
+    "unit-case": "lower",
+    "value-no-vendor-prefix": true
+  }
+}
