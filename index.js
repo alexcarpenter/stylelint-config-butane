8707eb6408777c240cca50e4d76a7cab4652734d
Alex Carpenter <alex@mightyinthemidwest.com>
2018-03-01 09:04:40 -0500

    Updates config to use standard as base

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 module.exports = {
-  "plugins": [
-    "stylelint-scss"
-  ],
+  "plugins": ["stylelint-scss"],
+  "extends": "stylelint-config-standard",
   "rules": {
+    "indentation": 2,
+    "string-quotes": "single",
+    "no-duplicate-selectors": true,
     "color-hex-case": "lower",
     "color-hex-length": "short",
+    "color-named": "never",
+    "selector-no-qualifying-type": true,
+    "selector-no-id": true,
+    "selector-combinator-space-after": "always",
+    "selector-attribute-quotes": "always",
+    "selector-attribute-operator-space-before": "always",
+    "selector-attribute-operator-space-after": "always",
+    "selector-attribute-brackets-space-inside": "never",
+    "declaration-block-trailing-semicolon": "always",
+    "declaration-colon-space-before": "never",
+    "declaration-colon-space-after": "always",
+    "property-no-vendor-prefix": true,
+    "value-no-vendor-prefix": true,
+    "number-leading-zero": "always",
+    "function-url-quotes": "always",
     "font-weight-notation": "numeric",
-    "indentation": 2,
-    "max-empty-lines": 4,
-    "max-nesting-depth": 2,
-    "no-invalid-double-slash-comments": true,
-    "string-quotes": "single",
-    "unit-case": "lower",
-    "value-no-vendor-prefix": true
+    "font-family-name-quotes": "always-where-recommended",
+    "comment-whitespace-inside": "always",
+    "comment-empty-line-before": "always",
+    "at-rule-no-vendor-prefix": true,
+    "rule-empty-line-before": "always",
+    "selector-pseudo-element-colon-notation": "single",
+    "selector-pseudo-class-parentheses-space-inside": "never",
+    "selector-no-vendor-prefix": true,
+    "media-feature-range-operator-space-before": "always",
+    "media-feature-range-operator-space-after": "always",
+    "media-feature-parentheses-space-inside": "never",
+    "media-feature-name-no-vendor-prefix": true,
+    "media-feature-colon-space-before": "never",
+    "media-feature-colon-space-after": "always"
   }
-}
+};
